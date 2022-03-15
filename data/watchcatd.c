8cde5fd7cb5d706f72ec8554a75356565c212730
pkseeg <m.parker.seegmiller@gmail.com>
2022-03-15 13:39:30 -0400

    everything up to date

--- data/watchcatd.c
+++
@@ -1,9 +0,0 @@
-/*
-** $Id: watchcatd.c 1654 2006-05-29 18:18:02Z andre $
-** watchcatd - Watchcat Daemon
-** See copyright notice in distro's COPYRIGHT file
-*/
-
-#include "watchcatd.h"
-
-WATCHCAT_GLOBAL_CONF GLOBAL_CONF;
